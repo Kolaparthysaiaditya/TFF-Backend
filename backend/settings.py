d4460e984120fea412362e927409cc4ac55407b6
Aditya <kolaparthysaiaditya@gmail.com>
2026-02-16 14:38:37 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -103,10 +103,10 @@ TEMPLATES = [
 # DATABASE (Local SQLite)
 # --------------------------------------------------
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 
