584642edc47673d723c36b9213d231ec2942636a
Aditya <kolaparthysaiaditya@gmail.com>
2026-02-17 14:52:23 +0530

    Remove scheduler startup from TffConfig
    Remove the ready method that starts the scheduler.

--- TFF/apps.py
+++ TFF/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class TffConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'TFF'
-
-    def ready(self):
-        from .tasks import start_scheduler
-        start_scheduler()
